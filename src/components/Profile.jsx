9e96672639e9bf5fb35ac135defc9784d5820572
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-11-05 20:24:18 +0300

    Начало проекта

---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className="content">
+            MainContent
+            <div>
+                <img src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg"></img>
+            </div>
+            <div>Ava + description</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
