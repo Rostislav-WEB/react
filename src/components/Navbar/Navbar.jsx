4723ba174989b41504ce42e31e8c3e5bad2da352
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-11-08 19:41:17 +0300

    add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,22 +3,35 @@ import { NavLink } from "react-router-dom";
 import s from './Navbar.module.css';
 
 const Navbar = () => {
+    let activeStyle = {
+        color: "red",
+    };
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <NavLink to="/profile" >Profile</NavLink>
+                <NavLink to="/profile" style={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                }>Profile</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/dialogs" >Messages</NavLink>
+                <NavLink to="/dialogs" style={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                }>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/news" >News</NavLink>
+                <NavLink to="/news" style={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                }>News</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/music">Music</NavLink>
+                <NavLink to="/music" style={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                }>Music</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/settings">Settings</NavLink>
+                <NavLink to="/settings" style={({ isActive }) =>
+                    isActive ? activeStyle : undefined
+                }>Settings</NavLink>
             </div>
         </nav>
     )
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import s from './Post.module.css';
 
 const Post = (props) => {
-    console.log(props);
     return (
         <div className={s.item}>
             <img src="https://www.meme-arsenal.com/memes/7bdea6754f999b50e9577596f09197fb.jpg" />
