cfed3d13aa7ee062d8c0446fb0c7373d63d424d7
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-11-10 16:51:57 +0300

    add map filter

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,27 +3,30 @@ import s from './MyPosts.module.css';
 import Post from "./Post/Post";
 
 const MyPosts = (props) => {
-    let postData = [
-        { id: 1, message: 'Rostislav222', likesCount: 12 },
-        { id: 2, message: '2222', likesCount: 15 },
+    let posts = [
+        { id: 1, message: 'Rostislav222', likesCount: 43 },
+        { id: 2, message: '34433434', likesCount: 15 },
+        { id: 3, message: 'blabla', likesCount: 2323 },
     ]
+    let postsElements = posts
+        .map(p => <Post message={p.message} likeCount={p.likesCount} />);
+
     return (
-            <div className={s.postsBlock}>
-                <h3>My posts</h3>
+        <div className={s.postsBlock}>
+            <h3>My posts</h3>
+            <div>
                 <div>
-                    <div>
-                        <textarea></textarea>
-                    </div>
-                   <div>
-                        <button>Add post</button>
-                   </div>
-                   
+                    <textarea></textarea>
                 </div>
-                <div className={s.posts}>
-                    <Post message={postData[0].message} likeCount={postData[0].likesCount} />
-                    <Post message={postData[1].message} likeCount={postData[1].likesCount} />
+                <div>
+                    <button>Add post</button>
                 </div>
+
             </div>
+            <div className={s.posts}>
+                {postsElements}
+            </div>
+        </div>
     )
 }
 
