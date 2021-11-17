7c1c77f71a18b016272c04f5061fba611e11e0b6
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-11-17 15:42:01 +0300

    add router render

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
+let SomeComponent;
+
+const App = (props) => {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
@@ -14,8 +16,10 @@ const App = () => {
         <Navbar />
         <div className="app-wrapper-content">
           <Routes>
-            <Route path="/profile" element={ <Profile /> } />
-            <Route exact path="/dialogs" element={ <Dialogs /> } />
+            {/* <Route path="/profile" element={ <Profile /> } />
+            <Route exact path="/dialogs" element={ <Dialogs /> } /> */}
+            <Route path="/dialogs" element={<Dialogs />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </div>
       </div>
