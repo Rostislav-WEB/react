54287dca47a328377837ef947c381aa500fbf326
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-11-20 17:15:32 +0300

    add state.js

--- src/index.js
+++ src/index.js
@@ -3,27 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-let posts = [
-  { id: 1, message: 'Rostislav222', likesCount: 43 },
-  { id: 2, message: '34433434', likesCount: 15 },
-  { id: 3, message: 'blabla', likesCount: 2323 },
-]
-let dialogs= [
-  { id: 1, name: 'Dima' },
-  { id: 2, name: 'Andrey' },
-  { id: 3, name: 'Valera' },
-  { id: 4, name: 'Sveta' },
-]
-let messages = [
-  { id: 1, message: 'hohoh' },
-  { id: 2, message: 'yoyoy' },
-  { id: 3, message: 'yo' },
-  { id: 4, message: 'sdds' },
-]
+import state from './redux/state';
 ReactDOM.render(
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages}/>
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/redux/state.js
@@ -0,0 +1,25 @@
+let state = {
+  profilePage: {
+    posts: [
+      { id: 1, name: "Dima" },
+      { id: 2, name: "Andrey" },
+      { id: 3, name: "Valera" },
+      { id: 4, name: "Sveta" }
+    ],
+  },
+  dialogsPage: {
+    messages: [  
+      { id: 1, message: "Rostislav222", likesCount: 43 },
+      { id: 2, message: "34433434", likesCount: 15 },
+      { id: 3, message: "blabla", likesCount: 2323 },
+    ],
+
+    dialogs: [
+      { id: 1, message: "hohoh" },
+      { id: 2, message: "yoyoy" },
+      { id: 3, message: "yo" },
+      { id: 4, message: "sdds" }
+    ]
+  }
+};
+export default state;
