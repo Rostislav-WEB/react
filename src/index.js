94926bfc07943cc023507611e9c0c5c0347fe9ef
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-11-26 14:42:41 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import reportWebVitals from "./reportWebVitals";
-import state from "./redux/state";
-import {rerenderEntireTree} from "./render.js"
+import state, { subscribe } from "./redux/state";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {addPost, updateNewPostText } from "./redux/state";
+import { BrowserRouter } from "react-router-dom";
+
+
+export let rerenderEntireTree = (state) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+      </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+}
+
 rerenderEntireTree(state);
 
+subscribe(rerenderEntireTree);
 
 reportWebVitals();
