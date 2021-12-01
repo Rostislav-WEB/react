4bfd7c3cc23460dd47b8f6ffb126f35558dcb5e6
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-12-01 20:42:34 +0300

    add action type and action creator

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,5 @@
+const ADD_POST = 'ADD_POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 let store = {
   _state: {
     profilePage: {
@@ -40,19 +42,29 @@ let store = {
     this.rerenderEntireTree = observer;
   },
   dispatch (action) {
-    if(action.type === 'ADD-POST') {
+    if(action.type === ADD_POST) {
       let newPost = {
         id: 5, message: this._state.profilePage.newPostText, likesCount: 0 
      }
      this._state.profilePage.posts.push(newPost);
      this._state.profilePage.newPostText = '';
      this.rerenderEntireTree(this._state);
-    } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
+    } else if (action.type === UPDATE_NEW_POST_TEXT) {
       this._state.profilePage.newPostText = action.newText;
       this.rerenderEntireTree(this._state);
     }
   },
 }
-
+export const addPostActionCreator = () => {
+  return {
+    type: ADD_POST,
+  }
+}
+export const updateNewPostTextActionCreator = (text) => {
+  return {
+    type: UPDATE_NEW_POST_TEXT,
+    newText: text,
+  }
+}
 
 export default store;
