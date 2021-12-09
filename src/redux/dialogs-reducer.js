abc310d3b3e4dc14002964cb97169a426465f3ed
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-12-09 22:08:53 +0300

    add dialogs clone obj

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -8,29 +8,30 @@ let initialState = {
     { id: 3, message: "qwerty3" },
     { id: 4, message: "qwerty4" },
   ],
-  newMessageBody: "",
   dialogs: [
     { id: 1, name: "Dima" },
     { id: 2, name: "Andrey" },
     { id: 3, name: "Valera" },
     { id: 4, name: "Sveta" },
   ],
+    newMessageBody: "",
 };
 const dialogsReducer = (state = initialState, action) => {
   switch (action.type) {
-    case UPDATE_NEW_MESSAGE_BODY:
-      state.newMessageBody = action.body;
-      return state;
-    case SEND_MESSAGE:
-      //   let newMessage = {
-      //     id: 5,
-      //     message: state.newMessageBody,
-      //   };
-      //   state.messages.push(newMessage);
-      let body = state.newMessageBody;
-      state.messages.push({ id: 5, message: body });
-      state.newMessageBody = "";
-      return state;
+    case UPDATE_NEW_MESSAGE_BODY: {
+      let stateCopy = {...state}
+      stateCopy.newMessageBody = action.body;
+      return stateCopy;
+    }
+    case SEND_MESSAGE: {
+      let stateCopy = {...state};
+      stateCopy.messages = [...state.messages]
+      let body = stateCopy.newMessageBody;
+      stateCopy.messages.push({ id: 5, message: body });
+      stateCopy.newMessageBody = "";
+      return stateCopy;
+    }
+      
     default:
       return state;
   }
