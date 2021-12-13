86bebd8da93a14c3b0e7af0f22ecab3a4d576de8
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-12-13 23:23:09 +0300

    add usersPage users reducer redux store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -2,10 +2,12 @@ import { combineReducers, createStore } from 'redux';
 import dialogsReducer from './dialogs-reducer';
 import profileReducer from './profile-reducer';
 import sidebarReducer from './sidebar-reducer';
+import usersReducer from './users-reducer';
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
+    usersPage: usersReducer
 })
 const store = createStore(reducers);
 
