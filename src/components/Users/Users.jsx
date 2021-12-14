25e0a8fdd9e07341e4047b6a5a1d5f47d539cf9e
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-12-14 11:31:57 +0300

    add axios запрос

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,27 +1,30 @@
 import React from "react";
-
+import * as axios from "axios";
+import userPhoto from '../../Accets/images/profile.png'
 const Users = (props) => {
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            props.setUsers(response.data.items)
+        })
+    }
     return (
         <div>
             {
                 props.users.map(user => <div key={user.id}>
                     <span>
                         <div>
-                            <img src={user.photoUrl} alt='Хуйня крч' />
+                            <img src={user.photos.small != null ? user.photos.small : userPhoto} alt=""  />
                         </div>
                         <div>
-                            {user.followed ? <button onClick={() => props.unFollow(user.id)}>unfollow</button> : 
-                            <button onClick={() => props.follow(user.id)}>follow</button>}
+                            {user.followed
+                                ? <button onClick={() => props.unFollow(user.id)}>unfollow</button>
+                                : <button onClick={() => props.follow(user.id)}>follow</button>}
                         </div>
                     </span>
                     <span>
                         <span>
-                            <div>{user.fullName}</div>
+                            <div>{user.name}</div>
                             <div>{user.status}</div>
-                        </span>
-                        <span>
-                            <div>{user.location.city}</div>
-                            <div>{user.location.country}</div>
                         </span>
                     </span>
                 </div>)
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,35 +2,7 @@ const FOLLOW = "FOLLOWER";
 const UNFOLLOW = "UNFOLOWER";
 const SET_USERS = "SET_USERS";
 let initialState = {
-  users: [
-    {
-      id: 1,
-      photoUrl:
-        "https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg",
-      followed: false,
-      fullName: "Dima",
-      status: "Hello i am Dima",
-      location: { city: "Говно", country: "Russia" },
-    },
-    {
-      id: 2,
-      photoUrl:
-        "https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg",
-      followed: true,
-      fullName: "Petya",
-      status: "Hello i am Petya",
-      location: { city: "Киев", country: "Russia" },
-    },
-    {
-      id: 3,
-      photoUrl:
-        "https://resizer.mail.ru/p/7a1e86df-9686-5098-963e-27304c6f1133/AAACY37OntEVxnf9OOUuLkcQpyEwVtb3AZgK14eU9OI6IljiYtBizDmEp-vG8UfPC3h-OB130PE-ba1mk1rY6S-3Zek.jpg",
-      followed: true,
-      fullName: "Rostislav",
-      status: "Hello i am Rostislav",
-      location: { city: "Maykop", country: "Russia" },
-    },
-  ],
+  users: [],
 };
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
