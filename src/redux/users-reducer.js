0130232b4745d3e44d97bfafe593e02b47dbd2eb
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-12-18 12:19:19 +0300

    add state, add dispatch

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,8 +1,17 @@
 const FOLLOW = "FOLLOWER";
 const UNFOLLOW = "UNFOLOWER";
 const SET_USERS = "SET_USERS";
+const SET_CURRRENT_PAGE = "SET_CURRRENT_PAGE";
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
 let initialState = {
+  // массив с пользователями
   users: [],
+  // pageSize - сколько выводится человек
+  pageSize: 5,
+  // общее количество
+  totalUsersCount: 0,
+  // текущая страница
+  currentPage: 1,
 };
 const usersReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -29,8 +38,19 @@ const usersReducer = (state = initialState, action) => {
     case SET_USERS:
       return {
         ...state,
-        users: action.users,
+        users: [ ...action.users],
       };
+      case SET_CURRRENT_PAGE:
+        return {
+          ...state,
+          currentPage: action.currentPage,
+          
+        };
+        case SET_TOTAL_USERS_COUNT:
+          return {
+            ...state,
+            totalUsersCount: action.count,
+          };
 
     default:
       return state;
@@ -39,6 +59,8 @@ const usersReducer = (state = initialState, action) => {
 
 export const followAC = (usersId) => ({ type: FOLLOW, usersId });
 export const unFollowAC = (usersId) => ({ type: UNFOLLOW, usersId });
-export const setUSersAC = (users) => ({ type: SET_USERS, users });
+export const setUsersAC = (users) => ({ type: SET_USERS, users });
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRRENT_PAGE, currentPage})
+export const totalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
 
 export default usersReducer;
