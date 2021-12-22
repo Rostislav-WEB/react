93b5e76244521f9d04627dff7ab44a3d0d5dbbc9
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-12-22 16:26:45 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
 import UsersContainer from "./components/Users/UsersContainer"
-import { Routes, Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 const App = (props) => {
   return (
@@ -16,7 +16,7 @@ const App = (props) => {
       <div className="app-wrapper-content">
         <Routes>
           <Route path="/dialogs/*" element={<DialogsContainer />} />
-          <Route path="/profile/*" element={<Profile />} />
+          <Route path="/profile/*" element={<ProfileContainer />} />
           <Route path="/users" element={<UsersContainer />} />
         </Routes>
       </div>
