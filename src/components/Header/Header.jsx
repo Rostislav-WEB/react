9956907071eaa6d08f2d0be7285777092dfec1f5
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-12-30 14:40:24 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,6 @@ const Header = (props) => {
                 { props.isAuth ? props.login 
                 : <NavLink to="/login">Login</NavLink>
                 }
-               
             </div>
         </header>
     )
