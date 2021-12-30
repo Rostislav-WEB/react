3330b534e1425dbc93e6a6faa69954616640d981
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-12-30 18:39:46 +0300

    remove class component add use effect functions compoonent

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { setAuthUserData } from "../../redux/auth-reducer";
+import { connect } from "react-redux";
+import axios from "axios";
 import { NavLink } from 'react-router-dom'
 import s from './Header.module.css';
 
 const Header = (props) => {
+    debugger;
+    useEffect(() => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
+            withCredentials: true
+        }).then(response => {
+          if (response.data.resultCode === 0) {
+              let {id, login, email} = response.data.data
+              props.setAuthUserData(id, email, login)
+          }
+        });
+      });
     return (
         <header className={s.header}>
             <img src="https://logos-download.com/wp-content/uploads/2016/09/React_logo_wordmark-700x235.png" alt='' />
@@ -14,5 +28,9 @@ const Header = (props) => {
         </header>
     )
 }
-
-export default Header;
+const mapStateToProps = (state) => { 
+    return {
+    isAuth: state.auth.isAuth,
+    login: state.auth.login }
+}
+export default connect(mapStateToProps, {setAuthUserData})(Header);
