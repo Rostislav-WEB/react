82fdcaa7d42525275c4f05be8680655b4e3e8d69
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2021-12-30 19:13:37 +0300

    add side effect profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { setUsersProfile } from "../../redux/profile-reducer";
+import { useMatch } from "react-router";
+import { connect } from "react-redux";
+import axios from "axios";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
-// import s from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
+
+
 const Profile = (props) => {
+    const match = useMatch('/profile/:userId/');
+    useEffect(() => {
+        let userId = match ? match.params.userId : 21535;
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then(response => {
+          props.setUsersProfile(response.data);
+        });
+    })
     return (
         <div>
             <ProfileInfo profile={props.profile} />
@@ -12,4 +24,10 @@ const Profile = (props) => {
     )
 }
 
-export default Profile;
+const mapStateToProps = (state) => {
+    return {
+        profile: state.profilePage.profile,
+    }
+}
+export default connect(mapStateToProps, {setUsersProfile})(Profile);
+
