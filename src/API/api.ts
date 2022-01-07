c2efb1d820ac26cb052940ee5c1593e4635809b5
Rostislav <77685422+Rostislav-WEB@users.noreply.github.com>
2022-01-07 22:40:03 +0300

    add put get

--- src/API/api.ts
+++ src/API/api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL: "https://social-network.samuraijs.com/api/1.0/",
   withCredentials: true,
   headers: {
-    "API-KEY": "cdd52f59-dcf4-4c84-8c5b-9a2ccb65238e",
+    "API-KEY": "b23b9336-16ae-49ae-8453-8ca99a2f1695",
   },
 });
 
@@ -19,17 +19,28 @@ export const usersAPI = {
   },
   unFollowSuccess(userId: number) {
     return instance
-    .delete(`follow/${userId}`);
+      .delete(`follow/${userId}`);
   },
 }
 
 export const profileAPI = {
-  
+  getProfile(userId: number) {
+    return instance
+      .get(`profile/${userId}`);
+  },
+  getStatus(userId: number) {
+    return instance
+      .get(`profile/status/${userId}`);
+  },
+  updateStatus(status: string) {
+    return instance
+      .put(`profile/status`, { status: status });
+  }
 }
 
 export const authAPI = {
   me() {
-      return instance
-        .get('auth/me');
+    return instance
+      .get('auth/me');
   }
 }
